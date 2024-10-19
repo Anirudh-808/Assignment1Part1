d2dc3d84a8cc0bf563b5ff3a8ad6090dad9d57bc
Anirudh-808 <aninagaraj808@gmail.com>
2024-10-19 19:42:24 +0530

    4

--- serial/serial.go
+++ serial/serial.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
 	//importing the bufio module(used ChatGPT for this)
 	//used for reading the file
 	"bufio"
@@ -18,14 +20,17 @@ func main() {
 	//the below is used to import the file big.txt
 	//Using Open and nor Readfile due to the bufio module(ChatGPT again)
 	dat, _ := os.Open("./big.txt")
-
+	cnt := 0
 	//the Newscanner function creates a new scanner to scan the doc
 	//it defaults to reading line by line
 	scanner := bufio.NewScanner(dat)
 	//iterate over the data in file and check
 	for scanner.Scan() {
 		line := scanner.Text()
-		vowel.CheckifVowelSerial(line)
+		result := vowel.CheckifVowelSerial(line)
+		cnt += result
 	}
+
+	fmt.Println(cnt)
 
 }
