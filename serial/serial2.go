9a4efc11e510fe58e5a7bdb50a40bc766cf897e0
Anirudh-808 <aninagaraj808@gmail.com>
2024-10-22 20:48:37 +0530

    2

--- serial/serial2.go
+++ serial/serial2.go
@@ -9,6 +9,8 @@ import (
 
 	//used the bufio module to scan line by line
 	"bufio"
+
+	"log"
 )
 
 // func CheckIfVowel(s string) int {
@@ -26,19 +28,22 @@ import (
 func main() {
 	dat, _ := os.Open("./big.txt")
 	count := 0
-	L := []string{"a", "e", "i", "o", "u"}
+	vowels := "aeiou"
+	defer dat.Close()
 
 	scanner := bufio.NewScanner(dat)
 	line := ""
 	for scanner.Scan() {
 		line = scanner.Text() // Get the current line as a string
-		for _, a := range line {
-			for _, l := range L {
-				if l == strings.ToLower(string(a)) {
-					count++
-				}
+		for _, char := range line {
+			if strings.ContainsRune(vowels, strings.ToLower(char)) {
+				count++
 			}
 		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err) // Handle any errors that occurred during scanning
 	}
 
 	fmt.Println(count)
